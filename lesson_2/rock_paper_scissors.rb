b991e946b1a26683df9b92e7ac4a220d8689a0ab
Cassie Fox <cssndrmfox@gmail.com>
2022-02-26 07:03:25 +0000

    Finished Rock, Paper, Scissors, Lizard, Spock

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,6 +1,11 @@
 # rock_paper_scissors.rb
 
-VALID_CHOICES = %w(rock paper scissors lizard spock)
+VALID_CHOICES = { r: 'rock',
+                  p: 'paper',
+                  s: 'scissors',
+                  l: 'lizard',
+                  sp: 'spock' }
+
 HOW_TO_WIN = { rock: %w(scissors lizard),
                paper: %w(rock spock),
                scissors: %w(paper lizard),
@@ -17,13 +22,28 @@ end
 
 def results(player, computer)
   if win?(player, computer)
-    "You won!"
+    'You won!'
   elsif win?(computer, player)
-    "Computer won!"
+    'Computer won!'
   else
     "It's a tie!"
   end
 end
+
+puts "Welcome to Rock, Paper, Scissors, Lizard, Spock!"
+puts "\n"
+
+puts 'The rules go like this: Scissors cuts Paper covers Rock crushes Lizard ',
+     'poisons Spock smashes Scissors decapitates Lizard eats Paper disproves ',
+     "Spock vaporizes Rock crushes Scissors."
+puts "\n"
+sleep(3)
+
+puts '... Got all that?'
+sleep(1)
+
+puts "Let's play!"
+puts "\n"
 
 player_counter = 0
 comp_counter = 0
@@ -31,36 +51,53 @@ comp_counter = 0
 loop do
   choice = ''
   loop do
-    prompt "Choose one: #{VALID_CHOICES.join(', ')} "
-    choice = gets.chomp.downcase
+    prompt <<-MSG
+    Choose one of the following options:
+      - rock (enter 'r')
+      - paper (enter 'p')
+      - scissors (enter 's')
+      - lizard (enter 'l')
+      - spock (enter 'sp')
+    MSG
+    choice = gets.chomp.downcase.to_sym
 
     if VALID_CHOICES.include?(choice)
       break
     else
+      puts "\n"
       puts "That's not a valid choice."
     end
   end
 
-  computer_choice = VALID_CHOICES.sample
+  computer_choice = VALID_CHOICES.keys.sample
 
-  puts "You chose: #{choice}; Computer chose: #{computer_choice}"
+  puts "\n"
+  puts "You chose: #{VALID_CHOICES[choice]}; " \
+       "Computer chose: #{VALID_CHOICES[computer_choice]}"
+  puts "\n"
 
-  who_won = results(choice, computer_choice)
-  player_counter += 1 if who_won == "You won!"
-  comp_counter += 1 if who_won == "Computer won!"
+  who_won = results(VALID_CHOICES[choice], VALID_CHOICES[computer_choice])
+  player_counter += 1 if who_won == 'You won!'
+  comp_counter += 1 if who_won == 'Computer won!'
 
   puts who_won
+  puts "\n"
+
   puts "Your wins: #{player_counter}"
   puts "Computer wins: #{comp_counter}"
+  puts "\n"
 
   break if player_counter == 3 || comp_counter == 3
 
   prompt "Do you want to play again? "
   answer = gets.chomp.downcase
   break unless answer.start_with?('y')
+
+  puts "\n"
 end
 
 puts "You're the grand champion! Congrats!" if player_counter == 3
 puts "The computer is the grand champion!" if comp_counter == 3
+puts "\n"
 
 puts "Thank you for playing. Goodbye!"
